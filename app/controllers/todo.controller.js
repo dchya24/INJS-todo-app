a73fc23571da2dd64d829c8133780d104da61f4d
Cahya Dinar Prastyo <cahyadinar241@gmail.com>
2022-03-13 15:07:20 +0700

    create feature from create todo to database

--- app/controllers/todo.controller.js
+++ app/controllers/todo.controller.js
@@ -11,6 +11,26 @@ exports.getTodos = async (req, res) => {
   catch(err){
     console.log(err);
     res.status(500)
-    .json({ message: err});
+    .json({ message: err.message});
+  }
+}
+
+exports.createTodo = async (req, res) => {
+  try{
+    const name = req.body.name;
+
+    const query = {
+      text: 'INSERT INTO tb_todos(name) VALUES($1)',
+      values: [name]
+    }
+
+    const execSQL = await db.query(query);
+
+    if(execSQL) res.send({message: "Berhasil membuat todo!"});
+  }
+  catch(err){
+    console.log(err);
+    res.status(500)
+    .json({ message: err.message});
   }
 }
--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/todo.controller");
 
 router.get("/", controller.getTodos);
+router.post("/todo", controller.createTodo);
 
 module.exports = router;
